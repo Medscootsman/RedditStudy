4445a9d2859d90a8455d732286ed5f6fef6b8760
Thompus <thompus987@gmail.com>
2019-03-24 21:29:55 +0000

    Length score R

---
+++ scripts/length_score.R
@@ -0,0 +1,20 @@
+#Load Libraries
+#JSON
+library(jsonlite)
+#Plotting
+library(ggplot2)
+
+#Data folder location
+dataFolder = "../data/"
+#Subreddit to parse
+subreddit = "LateStageCapitalism_short.json"
+
+#Loads JSON file
+comments <- stream_in(file(paste(dataFolder,subreddit, sep = "")))
+
+#Plot graph
+plot <- ggplot(comments, aes(x=nchar(comments$body), y=comments$score))
+plot <- plot + geom_point()
+plot <- plot + stat_smooth(method="lm",col="red")
+plot <- plot + xlab('Comment Length (Number of Characters)') + ylab('Score')
+plot
