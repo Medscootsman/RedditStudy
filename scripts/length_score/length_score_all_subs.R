b589d858837fe80c7ee45fde3649ae079858ed56
Thompus <thompus987@gmail.com>
2019-03-25 16:34:04 +0000

    Fixed length_score_all_subs

--- scripts/length_score/length_score_all_subs.R
+++ scripts/length_score/length_score_all_subs.R
@@ -10,24 +10,26 @@ dataFolder = "../../data/"
 #Subreddits to parse
 subreddits = c("australia_short", "canada_short", "dankmemes_short", "FortNiteBR_short", "funny_short", "GlobalOffensive_short", "LateStageCapitalism_short", "politics_short", "PUBATTLEGROUNDS_short", "RocketLeague_short", "tf2_short", "The_Donald_short", "unitedkingdom_short")
 
-subs = c("LateStageCapitalism_short", "PUBATTLEGROUNDS_short")
-
 #Loads JSON files
-for(i in subs){
+for(i in subreddits){
   #Load in the file at index
   comments <- stream_in(file(paste(dataFolder, i, ".json", sep = "")))
+  print(i)
   
-  #Add the scores to the previous scores
+  #Add the rows to the previous rows
   if(exists("score")){
-    score <- score + comments$score
+    score <- c(score, comments$score)
+    body <- c(body, comments$body)
   }
   else{
     score <- comments$score
+    body <- comments$body
   }
-  
-  #Remove comments to save memory for next loaded subreddit
-  #rm(comments)
+
 }
+
+#Make comments now a data frame with score and body rows
+comments <- data.frame(score, body, stringsAsFactors = FALSE)
 
 #Plot graph
 plot <- ggplot(comments, aes(x=nchar(comments$body), y=comments$score))
