7a4e62d015df6d153e8411041c46877149dc2e02
Medscootsman <murraylyne2@Gmail.com>
2019-03-27 12:37:57 +0000

    predictivemodel wip

--- scripts/predictiveModel.r
+++ scripts/predictiveModel.r
@@ -44,45 +44,118 @@ meanscore = mean(comments_specifics$Score)
 
 comments_below0 <- subset(comments_specifics, Score < 0)
 
-comments_above30 <- subset(comments_specifics, Score >= 30 & Score <= 60)
+comments_low <- subset(comments_specifics, Score > 0 & Score < 50)
 
-comments_low <- subset(comments_specifics, Score > 0 & Score < 20)
+comments_high <- subset(comments_specifics, Score >= 50)
 
-comments_high <- subset(comments_specifics, Score >= 20 & Score <= 100)
-
-comments_100 <- subset(comments_specifics, Score > 100)
 
 #turn it into a corpus
-body = Corpus(VectorSource(comments_100$Text))
+corpus_High = Corpus(VectorSource(comments_high$Text))
 
 
 #clean it up
-body = tm_map(body, tolower)
+corpus_High = tm_map(corpus_Low
+                    , tolower)
 
-body = tm_map(body, removePunctuation)
+corpus_High = tm_map(corpus_Low
+                    , removePunctuation)
 
-body = tm_map(body, removeWords, stopwords("english"))
+corpus_High= tm_map(corpus_High
+                    , removeWords, stopwords("english"))
 
 #stemming
-body = tm_map(body, stemDocument)
+corpus_High= tm_map(corpus_High
+                    , stemDocument)
 
-body = tm_map(body, function(x) iconv(enc2utf8(x), sub = "byte"))
+corpus_High= tm_map(corpus_High
+                    , function(x) iconv(enc2utf8(x), sub = "byte"))
 
-body = tm_map(body, removeWords, c("im", "myðŸ", ",", "f", "u", "'", "½hel", "s", "t", "got", "day", "lol", "ã‚„", "x200b", "also", "its", "&qt", "gt", "just", "now", "like", "see", "know", "way", "get", "that", "use", "want", "can", "dont", "one", "say", "even", "thing", "go"))
+corpus_High= tm_map(corpus_High
+                    , removeWords, c("im", "myðŸ", ",", "f", "u", "'", "½hel", "s", "t", "got", "day", "lol", "ã‚„", "x200b", "also", "its", "&qt", "gt", "just", "now", "like", "see", "know", "way", "get", "that", "use", "want", "can", "dont", "one", "say", "even", "thing", "go"))
 
 
 #str_replace_all(body, "â", "")
 
-term_count <- freq_terms(body, 20)
+#turn it into a corpus for low comments now
+corpus_Low= Corpus(VectorSource(comments_below0$Text))
+
+
+#clean it up
+corpus_Low= tm_map(corpus_Low, tolower)
+
+corpus_Low= tm_map(corpus_Low
+                    , removePunctuation)
+
+corpus_Low= tm_map(corpus_Low
+                    , removeWords, stopwords("english"))
+
+#stemming
+corpus_Low= tm_map(corpus_Low
+                    , stemDocument)
+
+corpus_Low= tm_map(corpus_Low
+                    , function(x) iconv(enc2utf8(x), sub = "byte"))
+
+corpus_Low= tm_map(corpus_Low
+                    , removeWords, c("im", "myðŸ", ",", "f", "u", "'", "½hel", "s", "t", "got", "day", "lol", "ã‚„", "x200b", "also", "its", "&qt", "gt", "just", "now", "like", "see", "know", "way", "get", "that", "use", "want", "can", "dont", "one", "say", "even", "thing", "go"))
+
+
+#str_replace_all(body, "â", "")
+
+#Low comments
+
+term_count <- freq_terms(corpus_Low, 20)
 
 term_count = subset(term_count, WORD != "ã‚„")
 
 plot(term_count)
 
-body_tdm <- TermDocumentMatrix(body)
+body_tdm <- TermDocumentMatrix(corpus_Low)
 
-body_dtm <- DocumentTermMatrix(body)
+body_dtm <- DocumentTermMatrix(corpus_Low)
+
+#high comments
+
+term_count <- freq_terms(corpus_High, 20)
+
+term_count = subset(term_count, WORD != "ã‚„")
+
+plot(term_count)
+
+body_tdm <- TermDocumentMatrix(corpus_High)
+
+body_dtm <- DocumentTermMatrix(corpus_High)
 
 
 #creating term freq
 
+bodyMatrix <- as.matrix(body_tdm)
+
+body_term_freq <- rowSums(bodyMatrix)
+
+body_term_freq <- sort(body_term_freq, decreasing = T)
+
+body_term_freq[1:10]
+
+#Combine them
+
+highcorp <- paste(corpus_High, collapse = "")
+
+lowcorp <- paste(corpus_Low, collapse = "")
+
+combined_corpus <- c(highcorp, lowcorp)
+
+combined_corpus=Corpus(VectorSource(combined_corpus)) 
+
+combined_corpus = tm_map(combined_corpus, tolower)
+
+combined_tdm <- TermDocumentMatrix(combined_corpus)
+
+combined_m = as.matrix(combined_tdm)
+
+colnames(combined_m)=c("High score", "Low score")
+
+#commonality cloud 
+commonality.cloud(combined_m,
+                  colors = "red",
+                  max.words = 50)
